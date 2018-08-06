e9d70e6259333da3b39d46d1055c222a72881dd5
ouo5566 <ouo5566@gmail.com>
2018-08-06 09:31:28 +0900

    home..

--- src/template/PstmtQuery.java
+++ src/template/PstmtQuery.java
@@ -10,16 +10,6 @@ public class PstmtQuery extends QueryTemplate{
 
 	@Override
 	void initialize() {
-		/*map.put("sql", String.format(
-				" SELECT "
-				+ ColumnFinder.find(Domain.MEMBER).toUpperCase()
-				+ " FROM %s "
-				+ " WHERE "
-				+ "	%s LIKE ? ",
-					map.get("table"),
-					map.get("column")));*/
-		
-		System.out.println("query: "+map.get("query").toString());
 		map.put("sql", 
 				QueryFactory.createQuery(
 						MemberQuery.valueOf(map.get("query").toString()), 
@@ -32,8 +22,6 @@ public class PstmtQuery extends QueryTemplate{
 		try {
 			pstmt = DatabaseFactory.createDatabase2(map).getConnection().prepareStatement((String) map.get("sql"));
 			//prepareStatement.setString()...은 void
-			System.out.println("sql : "+ map.get("sql").toString());
-			System.out.println("setString : "+"%"+map.get("value")+"%");
 			if(!map.get("value").toString().equals("")) {
 				pstmt.setString(1,
 						"%"+map.get("value")+"%");
@@ -48,23 +36,32 @@ public class PstmtQuery extends QueryTemplate{
 	void endPlay() {
 		try {
 			ResultSet rs = pstmt.executeQuery();
-			MemberBean mem = null;
-			while(rs.next()) {
-				mem = new MemberBean();
-				mem.setMemberId(rs.getString("MEMBER_ID"));
-				mem.setTeamId(rs.getString("TEAM_ID"));
-				mem.setName(rs.getString("NAME"));
-				mem.setSsn(rs.getString("SSN"));
-				mem.setRoll(rs.getString("ROLL"));
-				mem.setPassword(rs.getString("PASSWORD"));
-				mem.setGender(rs.getString("GENDER"));
-				mem.setAge(rs.getString("AGE"));
-				list.add(mem);
+			switch (MemberQuery.valueOf(map.get("query").toString())) {
+			case COUNT :
+				if(rs.next()) {
+					map.put("count", rs.getString("NMEMBER"));
+				}
+				break;
+			case SELECT :
+				MemberBean mem = null;
+				while(rs.next()) {
+					mem = new MemberBean();
+					mem.setMemberId(rs.getString("MEMBER_ID"));
+					mem.setTeamId(rs.getString("TEAM_ID"));
+					mem.setName(rs.getString("NAME"));
+					mem.setSsn(rs.getString("SSN"));
+					mem.setRoll(rs.getString("ROLL"));
+					mem.setPassword(rs.getString("PASSWORD"));
+					mem.setGender(rs.getString("GENDER"));
+					mem.setAge(rs.getString("AGE"));
+					list.add(mem);
+				}
+				break;
+			default:
+				break;
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
 	}
-
 }
