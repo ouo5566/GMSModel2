8f278b32433c93ae8f9da08e24f8ced99160d9bc
ouo5566 <ouo5566@gmail.com>
2018-08-07 15:06:40 +0900

    proxy ing

---
+++ src/proxy/PageProxy.java
@@ -0,0 +1,12 @@
+package proxy;
+
+import java.util.Map;
+
+public class PageProxy implements Proxy{
+
+	@Override
+	public Map<?, ?> carryOut(Map<?, ?> param) {
+		return null;
+	}
+
+}
---
+++ src/proxy/Proxy.java
@@ -0,0 +1,7 @@
+package proxy;
+
+import java.util.Map;
+
+public interface Proxy {
+	public Map<?, ?> carryOut(Map<?, ?> param);
+}
