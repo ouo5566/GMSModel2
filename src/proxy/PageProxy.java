5561c2f7a8f94f665698d57940a34f5d1f5e3072
ouo5566 <ouo5566@gmail.com>
2018-08-07 15:15:48 +0900

    proxy pattern 진행중

--- src/proxy/PageProxy.java
+++ src/proxy/PageProxy.java
@@ -6,6 +6,10 @@ public class PageProxy implements Proxy{
 
 	@Override
 	public Map<?, ?> carryOut(Map<?, ?> param) {
+		request.setAttribute("nextPage", (nextPage > 0)); 
+		request.setAttribute("prevPage", (prevPage > 0));
+		request.setAttribute("endPage", (nextPage > blockSize) ? block * blockSize : countPage );
+		request.setAttribute("beginPage", block * blockSize - bGap);
 		return null;
 	}
 
